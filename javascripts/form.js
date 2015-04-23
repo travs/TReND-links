270e8bc67e047b6fc222a42b107ea172a34e0943
Travis Jacobs <trav221@gmail.com>
2015-04-23 19:45:10 -0230

    Add stylized form with bootstrap-treepicker set with PLOS data.

---
+++ javascripts/form.js
@@ -0,0 +1,58 @@
+//field contents manager for research fields
+
+var limit = 3;
+var field = new Object();
+field.contents = [];
+field.add = function(text){
+  this.contents.push(text);
+}
+field.remove = function(text){
+  var i = this.contents.indexOf(text);
+  if (i !== -1){
+    this.contents.splice(i, 1);
+  }
+}
+field.asString = function(){
+  return this.contents.join(', ');
+}
+
+//creating the treeview
+
+$.getJSON('https://cdn.rawgit.com/travs/PLOS-Subject-Area-Explorer/master/plosthes.2014-5.json', function(data){
+  $('#fields').after('<div id="tree"></div>');
+  $('.fieldsInput').attr('readonly', '');
+  $('#tree').treeview({data: data, levels: 1, nodeIcon: 'glyphicon', multiSelect: true, highlightSelected: true});
+
+  //selecting adds to text field
+  $('#tree').on('nodeSelected', function(event, node){
+    if(field.contents.length === limit){
+      //immediately deselect node if at limit
+      $('#tree').treeview('unselectNode', [ node.nodeId ]);
+    }
+    else{
+      field.add(node.text[0]);
+      $('.fieldsInput').attr('value', field.asString());
+    }
+  })
+
+  //deselecting removes from text field
+  $('#tree').on('nodeUnselected', function(event, node){
+    field.remove(String(node.text[0]));
+    $('.fieldsInput').attr('value', field.asString());
+  })
+
+})
+
+//styling the form a little
+
+$('.ss-form-question').addClass('form-group');
+
+$('.required-message').remove();
+
+$('.error-message').remove();
+
+$('.ss-password-warning').remove();
+
+$('.ss-q-short, .ss-q-long').addClass('pull-right');
+
+$('select').addClass('pull-right');
