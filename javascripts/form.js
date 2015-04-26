0591a6e2158c27eae57b4d4281cb8368122ea34e
Travis Jacobs <trav221@gmail.com>
2015-04-26 15:27:22 -0230

    Fix link to subject area picker to point to .min.json

--- javascripts/form.js
+++ javascripts/form.js
@@ -20,69 +20,73 @@ field.asString = function(){
 var allowedFields = ['Biology and life sciences', 'Ecology and environmental sciences', 'Medicine and health sciences'];
 
 //creating the treeview
-$.getJSON('https://cdn.rawgit.com/travs/PLOS-Subject-Area-Explorer/master/plosthes.2014-5.json', function(data){
-  //remove broad scientific fields we're not allowing
-  for (var i = data.length - 1; i >= 0; i--){
-    //start at array end to deal with array resizing
-    var subtree = data[i];
-    var pos = allowedFields.indexOf(subtree.text[0]);
-    if(pos === -1){
-      //field not allowed
-      data.splice(pos, 1);
-    }
-  }
 
-  //only go a few levels (maxDepth) deep in the tree
-  var maxDepth = 3;
-  function traverse(tree, depth){
-    //recursively traverse tree
-    depth++;
-    if(depth < maxDepth){
-      for(var i = 0; i < tree.length; i++){
-        if(tree[i].nodes){
-          traverse(tree[i].nodes, depth);
+var sha = jQuery.ajax({url: 'https://api.github.com/repos/travs/PLOS-Subject-Area-Explorer/commits', success: function(data){
+  var sha = data[0].sha;
+  var URL = 'https://cdn.rawgit.com/travs/PLOS-Subject-Area-Explorer/' + sha + '/thesaurus_latest.min.json';
+  $.getJSON(URL, function(data){
+    //remove broad scientific fields we're not allowing
+    for (var i = data.length - 1; i >= 0; i--){
+      //start at array end to deal with array resizing
+      var subtree = data[i];
+      var pos = allowedFields.indexOf(subtree.text[0]);
+      if(pos === -1){
+        //field not allowed
+        data.splice(pos, 1);
+      }
+    }
+
+    //only go a few levels (maxDepth) deep in the tree
+    var maxDepth = 3;
+    function traverse(tree, depth){
+      //recursively traverse tree
+      depth++;
+      if(depth < maxDepth){
+        for(var i = 0; i < tree.length; i++){
+          if(tree[i].nodes){
+            traverse(tree[i].nodes, depth);
+          }
+          //make level 1 items unselectable
+          if(depth === 1){
+            tree[i].selectable = false;
+          }
         }
-        //make level 1 items unselectable
-        if(depth === 1){
-          tree[i].selectable = false;
+      }
+      else{
+        //we're at the maxDepth, so remove any nodes deeper than this
+        for(var i = 0; i < tree.length; i++){
+          if(tree[i].nodes){
+            delete tree[i].nodes;
+          }
         }
       }
     }
-    else{
-      //we're at the maxDepth, so remove any nodes deeper than this
-      for(var i = 0; i < tree.length; i++){
-        if(tree[i].nodes){
-          delete tree[i].nodes;
-        }
+
+    traverse(data, 0);
+
+    $('#fields').after('<div id="tree"></div>');
+    $('.fieldsInput').attr('readonly', '');
+    $('#tree').treeview({data: data, levels: 1, nodeIcon: 'glyphicon', multiSelect: true, highlightSelected: true});
+
+    //selecting adds to text field
+    $('#tree').on('nodeSelected', function(event, node){
+      if(field.contents.length === limit){
+        //immediately deselect node if at limit
+        $('#tree').treeview('unselectNode', [ node.nodeId ]);
       }
-    }
-  }
+      else{
+        field.add(node.text[0]);
+        $('.fieldsInput').attr('value', field.asString());
+      }
+    })
 
-  traverse(data, 0);
-
-  $('#fields').after('<div id="tree"></div>');
-  $('.fieldsInput').attr('readonly', '');
-  $('#tree').treeview({data: data, levels: 1, nodeIcon: 'glyphicon', multiSelect: true, highlightSelected: true});
-
-  //selecting adds to text field
-  $('#tree').on('nodeSelected', function(event, node){
-    if(field.contents.length === limit){
-      //immediately deselect node if at limit
-      $('#tree').treeview('unselectNode', [ node.nodeId ]);
-    }
-    else{
-      field.add(node.text[0]);
+    //deselecting removes from text field
+    $('#tree').on('nodeUnselected', function(event, node){
+      field.remove(String(node.text[0]));
       $('.fieldsInput').attr('value', field.asString());
-    }
+    })
   })
-
-  //deselecting removes from text field
-  $('#tree').on('nodeUnselected', function(event, node){
-    field.remove(String(node.text[0]));
-    $('.fieldsInput').attr('value', field.asString());
-  })
-
-})
+}})
 
 //styling the form a little
 
