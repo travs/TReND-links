8242808105a9b98009c8182d6e5c8d9a1ab2919a
Travis Jacobs <trav221@gmail.com>
2015-06-23 22:35:32 -0230

    Fix routing to about page

--- app.py
+++ app.py
@@ -64,6 +64,10 @@ def login():
         flash("Your email or password doesn't match.", 'error')
   return render_template('login.html', form=form)
 
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
 @app.route('/logout')
 @login_required
 def logout():
